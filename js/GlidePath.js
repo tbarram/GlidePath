a1dbe5dcf1dd1231b4fee5d07e83fa5df6bdced7
ebarram <tbarram@tbarrams-MBP.attlocal.net>
2019-12-20 21:37:42 -0800

    Javascript passes by value

--- js/GlidePath.js
+++ js/GlidePath.js
@@ -1484,7 +1484,9 @@ let shuffleArray = function(a)
     for (let i = (a.length - 1); i > 0; i--) 
     {
         const j = Math.floor(Math.random() * (i + 1));
-        swap(a[i],a[j]);
+        const tmp = a[i];
+    	a[i] = a[j];
+    	a[j] = tmp;
     }
     return a;
 }
